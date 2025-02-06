acbc6fe9c02b54d2230f24abb21d9c389d258f5e
DESKTOP-92UBI0Q\user <ibrahimethemsancar@gmail.com>
2025-02-06 13:07:59 +0300

    add cart service and cart status component

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 
 @Component({
   selector: 'app-product-list',
@@ -22,7 +24,8 @@ export class ProductListComponent implements OnInit {
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private cartService : CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -115,6 +118,9 @@ export class ProductListComponent implements OnInit {
   }
   
   addToCart(theProduct : Product) {
-    console.log(`Adding to cart : ${theProduct.name}, ${theProduct.unitPrice}`)
+    console.log(`Adding to cart : ${theProduct.name}, ${theProduct.unitPrice}`);
+    
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
 }
