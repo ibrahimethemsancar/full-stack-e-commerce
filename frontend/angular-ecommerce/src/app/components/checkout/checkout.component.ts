1fc8637980b38a9035db26c285dccbcbfcca8474
DESKTOP-92UBI0Q\user <ibrahimethemsancar@gmail.com>
2025-02-21 09:33:56 +0300

    add check out component

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -7,18 +7,56 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-  checkoutFromGroup!: FormGroup;
+  checkoutFormGroup!: FormGroup;
 
-  constructor(private formBuilder : FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.checkoutFromGroup = this.formBuilder.group({
-      customer : this.formBuilder.group({
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
       })
     })
+  }
+
+  copyShippingAddressToBillingAddress(event: any) {
+    console.log(this.checkoutFormGroup.controls);
+    if (event.target.checked) {
+      this.checkoutFormGroup.get('billingAddress')?.patchValue(
+        this.checkoutFormGroup.get('shippingAddress')?.value
+      );
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
+  onSubmit() {
+    console.log("press submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
   }
 
 }
